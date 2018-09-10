bc61dae1a3d828ff1bf37d1fa7c460b59fd710af
Yaser Adel Mehraban <adel.mehrban@gmail.com>
2018-09-10 20:54:56 +1000

    Removed Sw.js and added webpack workbox plugin

--- NDC.Workshop.Server/Program.cs
+++ NDC.Workshop.Server/Program.cs
@@ -17,8 +17,16 @@ namespace NDC.Workshop.Server
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())            
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .Build();
+
+            return WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+        }
+            
     }
 }
